2c63d8c4eb66d880fe7f999b7c72b209e4cefec7
RakeshJV2000 <rakeshjv2000@gmail.com>
2024-07-26 19:31:55 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+HYPHEN_E_DOT = '-e .'
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+__version__ = "0.0.1"
+
+REPO_NAME = "end-to-end-NLP"
+AUTHOR_USER_NAME = "Rakesh"
+SRC_REPO = "end-to-end-NLP-textSummarizer"
+AUTHOR_EMAIL = "rakeshjv2000@gmail.com"
+
+
+setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python web app for text summarization",
+    packages=find_packages()
+)
