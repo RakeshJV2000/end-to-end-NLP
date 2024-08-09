360c93e0c315483d7336c386749d638c7c113e5a
RakeshJV2000 <rakeshjv2000@gmail.com>
2024-08-09 17:52:04 +0530

    edit src

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from typing import Any
 
 
+
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """reads yaml file and returns
@@ -31,6 +32,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         raise ValueError("yaml file is empty")
     except Exception as e:
         raise e
+    
 
 
 @ensure_annotations
@@ -47,6 +49,7 @@ def create_directories(path_to_directories: list, verbose=True):
             logger.info(f"created directory at: {path}")
 
 
+
 @ensure_annotations
 def get_size(path: Path) -> str:
     """get size in KB
@@ -57,6 +60,7 @@ def get_size(path: Path) -> str:
     Returns:
         str: size in KB
     """
-    size_in_kb = round(os.path.getsize(path) / 1024)
+    size_in_kb = round(os.path.getsize(path)/1024)
     return f"~ {size_in_kb} KB"
 
+    
